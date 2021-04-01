66502e7884fed53d7f4c53ccb0707086df9afb6b
DcentLookingCat <67964470+DcentLookingCat@users.noreply.github.com>
2021-04-01 12:51:34 -0700

    Update Tax Amount

--- TRMDesktopUI.Library/Helpers/ConfigHelper.cs
+++ TRMDesktopUI.Library/Helpers/ConfigHelper.cs
@@ -5,6 +5,7 @@ namespace TRMDesktopUI.Library.Helpers
 {
     public class ConfigHelper : IConfigHelper
     {
+        // TODO: Move this from config to the API
         public decimal GetTaxRate()
         {
             string rateText = ConfigurationManager.AppSettings["taxRate"];
