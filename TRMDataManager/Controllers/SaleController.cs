c6ebe942660d307fc41a4f32cfb09518cc7ed6c1
DcentLookingCat <67964470+DcentLookingCat@users.noreply.github.com>
2021-04-11 21:19:03 -0700

    Add Admin Endpoint Data
    Adds the data for administrative endpoints.
    Does not yet secure the endpoints beyond authorization.

--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
 
 namespace TRMDataManager.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class SaleController : ApiController
     {
         public void Post(SaleModel sale)
@@ -14,6 +15,14 @@ namespace TRMDataManager.Controllers
             string userId = RequestContext.Principal.Identity.GetUserId();
 
             data.SaveSale(sale, userId);
+        }
+
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+
+            return data.GetSaleReport();
         }
     }
 }
